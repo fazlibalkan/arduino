778794fe000c945bca94cde84dcf2231fcb9ad20
fazlibalkan <53044560+fazlibalkan@users.noreply.github.com>
2020-12-28 02:22:05 +0300

    update

--- rc_servo_motor/rc_servo_motor.ino
+++ rc_servo_motor/rc_servo_motor.ino
@@ -2,13 +2,15 @@
 
 #define servoPin 8
 
-int canBeChosen[3] = {1, 2, 3};
+int canBeChosen[4] = {1, 2, 3, 4};
 int input;
+char rx_byte = 0;
+String rx_str = "";
+
 
 Servo servo;
 
 const byte servoP = A0;
-
 
 int servoVal;
 int pwm;
@@ -27,21 +29,71 @@ void setup()
 void loop()
 {
   input = GetInput(1);
-  /*
   if (input == 1) {
-    while (1) {
+    while (notyet) {
+      Serial.println("a");
       inputAngle = GetInput(0);
-      servo2(inputAngle);
-      
     }
-    
+    while(1) {
+      Serial.println("b");
+      servo1(inputAngle);
+    }
+    notyet = 1;
+  } else if (input == 2) {
+    while (notyet) {
+      Serial.println(inputAngle);
+      inputAngle = GetInput(0);
+    }
+    while(1) {
+      Serial.println(inputAngle);
+      servo2(inputAngle);
+    }
+    notyet = 1;
+  } else if (input == 3) {
+    while (1) {
+      ServoControlWithPOT();
+    }
+  } else if (input == 4) {
+    //CommunicationProtocol();
+    while (1) {
+      GetInputAsString();
+    }
+    Serial.println(rx_str);
   }
-  */
-  servo2(-45);
-  //servo1(-90);
-  //ServoControlWithPOT();
+  
+  
+  
 }
 
+void CommunicationProtocol(String str) {
+  String pos = "";
+  String wait = "";
+  int i = 1;
+  int posorwait = -1;
+  
+  while (1) {
+    if (str[i] == ':') {
+      posorwait = 1;
+    }
+    if (str[i] == ',') {
+      posorwait = -1;
+      servo2(int(pos));
+      delay(int(wait));
+    }
+    if (posorwait == -1) {
+      pos += str[i];
+    }
+    if (posorwait == 1) {
+      wait += str[i];
+    }
+    i++;
+  }
+}
+
+
+/*
+ *Control the servo with the potentiometer
+ */
 void ServoControlWithPOT() {
   servoVal = analogRead(servoP);
   int newVal = map(servoVal , 0, 1023, 0, 180);
@@ -66,7 +118,11 @@ void servo1 (int angle)
 
 void servo2 (int angle)
 {
-  servo.write(angle + 90);
+  if (angle >= -90 && angle <= 90) {
+    servo.write(angle + 90);
+  } else {
+    Serial.println("You wrote an invalid angle input. Please make sure that your input is in the range [-90, 90]");
+  }
 }
 
 
@@ -81,7 +137,7 @@ int GetInput(int first) {
     chosenNumber = Serial.parseInt(); // reading the input
 
     bool wrongInput = 1;          // checking if the user gave a correct input
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 4; i++) {
       if (chosenNumber == canBeChosen[i])
         wrongInput = 0;
     }
@@ -93,4 +149,23 @@ int GetInput(int first) {
     }
   }
   return chosenNumber;
+}
+
+void GetInputAsString() {
+  if (Serial.available() > 0) {    // is a character available?
+    rx_byte = Serial.read();       // get the character
+    Serial.print("Welcome ");
+    if (rx_byte != '\n') {
+      // a character of the string was received
+      rx_str = rx_str + rx_byte;
+    }
+    else {
+      // end of string
+      Serial.println("Welcome ");
+      Serial.println(rx_str);
+      //rx_str = "";                // clear the string for reuse
+      Serial.println("");
+      Serial.println("Enter your name.");
+    }
+  }
 }
